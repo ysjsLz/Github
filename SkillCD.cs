4ab33ae36d63bbffe9e27aae2b9a5d95a1ec33a6
jsysLz <798357885@qq.com>
2016-09-26 16:54:46 +0800

    提交
    你好

---
+++ SkillCD.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillCD : MonoBehaviour {
+	private float currentTime;
+	private float SkillTime1;
+	private float hasTime;
+	private bool isCanAttack=true;
+	public UISprite mySp;
+	public UILabel myLab;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = 0;
+		SkillTime1 = 5;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mySp.fillAmount.Equals(1)) {
+			return;
+		}
+		currentTime += Time.deltaTime;
+		hasTime = SkillTime1 - Mathf.FloorToInt (currentTime);
+		myLab.text = hasTime.ToString ();
+		mySp.fillAmount = 1 / SkillTime1 * currentTime;
+		if (currentTime>=SkillTime1) {
+			currentTime = 0;
+			isCanAttack = true;
+			myLab.enabled = false;
+			this.GetComponent<UIButton> ().enabled = true;
+		}
+	}
+
+	void OnClick(){
+		if (isCanAttack) {
+			mySp.fillAmount = 0;
+			isCanAttack = false;
+			myLab.enabled = true;
+			this.GetComponent<UIButton> ().enabled = false;
+		}
+
+	}
+}
